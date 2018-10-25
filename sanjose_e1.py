67eda54f84a6d3be249cdd1d6f212c96cb8a0da1
marietrisha <44455726+marietrisha@users.noreply.github.com>
2018-10-25 16:11:01 +0800

    Add files via upload

---
+++ sanjose_e1.py
@@ -0,0 +1,13 @@
+data = {"name": "Trisha Marie San Jose",
+"Spirit Animal": "Tiger",
+"Because": "I want to explore and discover new things",
+"Hobby": "I love to adventure",
+"Dream": "Seafarer in the future",
+}
+
+#print(data)
+print("I am {}".format(data["name"]))
+print("My spirit animal is {}".format(data["Spirit Animal"]))
+print("because {}".format(data["Because"]))
+print("When not in school, I love to {}".format(data["Hobby"]))
+print ("I dream of being a/an {}".format(data["Dream"]))
